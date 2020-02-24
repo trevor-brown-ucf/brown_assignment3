a9f28624e63c8c850f6af70b31f3378e88211ac0
Trevor Brown <trevor_brown@Knights.ucf.edu>
2020-02-23 21:08:58 -0500

    Finished and uploaded all java class files for problem 2

--- brown_problem2/src/SavingsAccount.java
+++ brown_problem2/src/SavingsAccount.java
@@ -1,4 +1,28 @@
 
-public class SavingsAccount {
+public class SavingsAccount								// "Create class SavingsAccount"
+{
+        static private double annualInterestRate;		// "Use a static variable annualInterestRate to store the annual interest rate for all account holders"
+        private double savingsBalance;					// "Each object of the class contains a private instance variable savingsBalance indicating the amount the saver currently has on deposit"
 
+        public void calculateMonthlyInterest()									// "Provide method calculateMonthlyInterest to calculate the monthly interest... "
+        {
+                double monthlyI;
+                monthlyI= (double)(this.savingsBalance*annualInterestRate/12);	// "... by multiplying the savingsBalance by annualInterestRate divided by 12"
+                this.savingsBalance+=monthlyI;									// "this interest should be added to savingsBalance"
+        }
+        
+        public static void modifyInterestRate(double newInterestRate)			// "Provide a static method modifyInterestRate that sets the annualInterestRate to a new value."
+        {
+                annualInterestRate=newInterestRate;
+        }
+        
+        public SavingsAccount(double savingBalance)		// Account constructor
+        {
+                this.savingsBalance=savingBalance;
+        }
+
+        public double getSavingBalance()				// method to read the balance
+        {
+                return this.savingsBalance;
+        }
 }
--- brown_problem2/src/SavingsAccountTest.java
+++ brown_problem2/src/SavingsAccountTest.java
@@ -1,4 +1,44 @@
 
-public class SavingsAccountTest {
+public class SavingsAccountTest 
+{
+	public static void main(String[] args) 
+	{
+		SavingsAccount saver1, saver2; 							// "Instantiate two SavingsAccount objects, saver1 and saver2"
+		saver1 = new SavingsAccount (2000.0);					// "with balances of $2000.00"
+		saver2 = new SavingsAccount (3000.0);					// "and $3000.00"
 
+		System.out.println("Rate of 4%\n");
+		SavingsAccount.modifyInterestRate (0.04);				// "Set annualInterestRate to 4%"
+		
+		System.out.println("\tSaver 1");
+		for(int i=0; i<12; i++) 								// "then calculate the monthly interest for each of 12 months and print the monthly balances for both savers"
+		{
+			saver1.calculateMonthlyInterest();       
+			System.out.println("Month " + (i+1)+": \t$"+ String.format("%.2f",saver1.getSavingBalance()));
+		}
+		
+		System.out.println("\n\tSaver 2");
+		for(int i=0; i<12; i++) 
+		{
+			saver2.calculateMonthlyInterest();
+			System.out.println("Month " + (i+1)+": \t$"+ String.format("%.2f",saver2.getSavingBalance()));
+		}
+
+		System.out.println("\n\nChanging rate to 5%\n");
+		SavingsAccount.modifyInterestRate(0.05);				// "Next, set the annualInterestRate to 5%"
+		
+		System.out.println("\tSaver 1");
+		for(int i=0; i<12; i++) 								// "calculate the next month’s interest and print the new balances for both savers."
+		{
+			saver1.calculateMonthlyInterest();            
+			System.out.println("Month " + (i+1)+": \t$"+ String.format("%.2f",saver1.getSavingBalance()));
+		}
+		
+		System.out.println("\n\tSaver 2");
+		for(int i=0; i<12; i++) 
+		{
+			saver2.calculateMonthlyInterest();
+			System.out.println("Month " + (i+1)+": \t$"+ String.format("%.2f",saver2.getSavingBalance()));
+		}
+	}
 }
